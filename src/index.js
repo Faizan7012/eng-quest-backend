720812203c75521e5a105a337b91035eaf33c0a5
Faizan7012 <frtoons7012@gmail.com>
2024-02-01 15:48:11 +0530

    new

--- src/index.js
+++ src/index.js
@@ -12,14 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://frontend-eight-phi-39.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    // ...
-    next();
-  });
-
 
 
 //routes
