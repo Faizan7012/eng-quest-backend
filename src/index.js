62a0669494b73f24468fb562bf2b6080f1417174
Faizan7012 <frtoons7012@gmail.com>
2024-02-01 17:00:49 +0530

    new

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: '*' }));
 app.use(morgan('combined'))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 
 
