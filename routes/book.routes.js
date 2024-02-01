ea55e8e017d910e29d71647d46a106ebb557ba69
Faizan7012 <frtoons7012@gmail.com>
2024-02-01 16:58:05 +0530

    new

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,13 +1,9 @@
 const express = require("express");
-const cors = require('cors')
 const { isCreator } = require("../middleware/creator");
 const booksModel = require("../models/book.model");
 const { isCreatordel } = require("../middleware/creatordel");
 const BookRouter = express.Router();
-BookRouter.options('/', cors());
-BookRouter.use(cors({
-  origin: 'http://localhost:3000',
-}));
+
 BookRouter.get("/", async (req, res) => {
     try {
 
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(morgan('combined'))
 
 
