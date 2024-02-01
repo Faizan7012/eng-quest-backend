c1f32dd9d9431e1174c93298c5289ecdcc10d22b
Faizan7012 <frtoons7012@gmail.com>
2024-02-01 15:28:22 +0530

    new

--- src/index.js
+++ src/index.js
@@ -10,12 +10,11 @@ const app = express();
 
 // middlewares
 app.use(express.json());
-const corsOrigin ={
-    origin:'https://frontend-eight-phi-39.vercel.app', //or whatever port your frontend is using
-    credentials:true,            
-    optionSuccessStatus:200
-}
-app.use(cors(corsOrigin));
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://frontend-eight-phi-39.vercel.app');
+    next();
+  });
 
 
 
